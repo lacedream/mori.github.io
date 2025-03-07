f34946da5fd34456e28d17e4f5414217b9f63682
lacedream <skxshi@gmail.com>
2025-03-07 06:46:05 +0000

    Add files via upload

--- events.js
+++ events.js
@@ -10,3 +10,12 @@ sets.forEach(image => {
         image.style.opacity = "2%";  
     });
 });
+
+const cookie = document.getElementById("cookie")
+var bite = new Audio('assets/music/chomp.mp3');
+
+cookie.addEventListener("click", function() {
+    bite.play();
+    cookie.src = "assets/bearcookieATED.png"
+
+})
